175918e55135ddfd44cbf34a77df8bd877f8921d
collin <munzmoses@gmail.com>
2021-02-03 11:23:48 -0500

    ummm

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 
-<<<<<<< HEAD
 import GameInfoCarousel from './components/GameInfoCarousel/GameInfoCarousel';
 import GlobalStyles from './components/styles/theme/globalStyles';
 import Theme from './components/styles/theme/theme';
@@ -11,20 +10,10 @@ if (module.hot) {
   module.hot.accept();
 }
 
-=======
-import GameInfoCarousel from './components/GameInfoCarousel';
-import GlobalStyles from './components/styles/theme/globalStyles';
-import Theme from './components/styles/theme/theme';
-
->>>>>>> main
 render(
   <ThemeProvider theme={Theme}>
     <GlobalStyles />
     <GameInfoCarousel />
   </ThemeProvider>,
-<<<<<<< HEAD
   document.getElementById('gameInfoCarousel'),
-=======
-  document.getElementById('root'),
->>>>>>> main
 );
