4fb0ad6d78c7df18b340150145b14315c9963d87
Collin <collinmcmahon2014@yahoo.com>
2021-02-18 23:13:01 -0500

    serviceRoutesUpdated

--- server/outgoingRoutes.js
+++ server/outgoingRoutes.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const getReviewsInfo = (gameId) => {
-  return axios.get(`http://localhost:3001/reviews?id=${gameId}`)
+  return axios.get(`http://204.236.178.72:3001/reviews?id=${gameId}`)
     .then((res) => {
       return res.data;
     })
@@ -11,7 +11,7 @@ const getReviewsInfo = (gameId) => {
 };
 
 const getMetaData = (gameId) => {
-  return axios.get(`http://localhost:3005/metadata?id=${gameId}`)
+  return axios.get(`http://3.131.140.35:3005/metadata?id=${gameId}`)
     .then((res) => {
       return res.data;
     })
